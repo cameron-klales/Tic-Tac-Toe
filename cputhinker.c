3221ebd3ec603ddb689912e476d637fcfbcd4937
cameron-klales <tuf62129@temple.edu>
2015-12-07 02:43:19 -0500

    Update cputhinker.c

--- cputhinker.c
+++ cputhinker.c
@@ -4,19 +4,25 @@ int win_chance(char board[], char j){
         for(i=0;i<3;i++);
         if(board[i]!=' '){
                 if(board[i]==board[i+1]{
-                board[i+2]=j;
+                if(board[i]==' '){
+                        board[i+2]=j;
+                        }
                 }
         }//checks for wins in c3
         for(i=0;i<3;i++);
         if(board[i]!=' '){
                 if(board[i]==board[i+2]{
-                board[i+1]=j;
+                if(board[i]==' '){
+                        board[i+1]=j;
+                        }
                 }
         }//checks for wins in c2
         for(i=0;i<3;i++);
         if(board[i+1]!=' '){
                 if(board[i+1]==board[i+2]{
-                board[i]=j;
+                        if(board[i]==' '){
+                        board[i]=j;
+                        }
                 }
         }//checks for wins in c1
         // break
@@ -24,7 +30,7 @@ int win_chance(char board[], char j){
         if(board[i]!=' '){
                 if(board[i]==board[i+3]{
                         if(board[i]==' '){
-                board[i+2]=j;
+                        board[i+2]=j;
                         }
                 }
         }//checks for wins in r3
