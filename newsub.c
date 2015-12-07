9ab9462570db51ac8e4a55b5758d5fca91a89550
cameron-klales <tuf62129@temple.edu>
2015-12-07 13:00:13 -0500

    Update newsub.c

--- newsub.c
+++ newsub.c
@@ -1,1 +1,26 @@
+#include<stdio.h>
+#include<stdlib.h>
+void blanker(char gamma[]);
+int main(void){
+        char board[9];
+        int i;
+        int z=1;
+        while (z==1)     
+        for(i=0;i<9;i++){
+                if (delta == 0){
+                
+                ++delta;
+                continue;
+                }else if(delta ==1){
+                /*do the other stuff*/
+                --delta;
+                continue;
+        }
 
+}
+void blanker(char gamma[]){
+        int i;
+        for(i=0;i<9;i++){
+                gamma[i]=' ';
+        }
+}
