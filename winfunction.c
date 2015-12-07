57a422b84ac4036bfd582ca61503d37514b2fcb5
cameron-klales <tuf62129@temple.edu>
2015-12-07 12:56:29 -0500

    Update winfunction.c

--- winfunction.c
+++ winfunction.c
@@ -40,47 +40,10 @@ int main(void){
 int winner_found(char board[]){
         int i;
         for(i=0;i<9;i+=3){
-                if (board[i]==board[i+1]){
-                        if (board[i+2]==board[i+1]){
-                                puts("Hwin");
-                        }
-                }
-        } // checks horizontal wins
-        for(i=0;i<3;i+=2){
-                if(board[i]==board[i+2]){
-                        if(board[i+2]==board[i+4])
-                        puts("Dwin");
-                }
-        } // checks diagonal wins
-        for(i=0;i<3;i++){
-                if (board[i]==board[i+3]){
-                        if (board[i+6]==board[i+3]){
-                                puts("Vwin");
-                        }
-                }
-        } // checks vertical wins
-
-
-astro.ocis.temple.edu> more wintest.c
-#include<stdio.h>
-#include<stdlib.h>
-
-int winner_found(char board[]);
-int main(void){
-        int i;
-        char gamma[9];
-        for(i=0;i<9;i++){
-                gamma[i]='a';
-        }
-        winner_found(gamma);
-}
-int winner_found(char board[]){
-        int i;
-        for(i=0;i<9;i+=3){
                 if(board[i]!=' '){
                         if (board[i]==board[i+1]){
                                 if (board[i+2]==board[i+1]){
-                                        puts("Hwin");
+                                        printf("The Winner is %c",board[i]);
                                 }
                         }
                 }
@@ -90,7 +53,7 @@ int winner_found(char board[]){
                 
                         if(board[i]==board[i+2]){
                                 if(board[i+2]==board[i+4])
-                                puts("Dwin");
+                                printf("The Winner is %c",board[i]);
                         }
                 
         } // checks diagonal wins
@@ -98,7 +61,7 @@ int winner_found(char board[]){
                 if(board[i]!=' '){
                         if (board[i]==board[i+3]){
                                 if (board[i+6]==board[i+3]){
-                                        puts("Vwin");
+                                        printf("The Winner is %c",board[i]);
                                 }
                         }
                 }
