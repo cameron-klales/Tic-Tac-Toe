bbed74b6a7b7c680a77b97bd8611523868ac46d5
cameron-klales <tuf62129@temple.edu>
2015-12-07 12:41:27 -0500

    Update movelogic.c

--- movelogic.c
+++ movelogic.c
@@ -1,1 +1,16 @@
+void movelogic(void){
 
+int delta = /*0 if user is x, 1 if user*/;
+
+for(i=0;i<9;i++){
+        if (delta == 0){
+                /*do the stuff*/
+                ++delta;
+                continue;
+        }else if(delta ==1){
+                /*do the other stuff*/
+                --delta;
+                continue;
+        }
+}
+}
