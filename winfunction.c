20795c335cf7ffb521ca93c4d4408d127df0bae5
cameron-klales <tuf62129@temple.edu>
2015-12-07 01:49:56 -0500

    Update winfunction.c

--- winfunction.c
+++ winfunction.c
@@ -77,23 +77,30 @@ int main(void){
 int winner_found(char board[]){
         int i;
         for(i=0;i<9;i+=3){
-                if (board[i]==board[i+1]){
-                        if (board[i+2]==board[i+1]){
-                                puts("Hwin");
+                if(board[i]!=' '){
+                        if (board[i]==board[i+1]){
+                                if (board[i+2]==board[i+1]){
+                                        puts("Hwin");
+                                }
                         }
                 }
         } // checks horizontal wins
         for(i=0;i<3;i+=2){
-                if(board[i]==board[i+2]){
-                        if(board[i+2]==board[i+4])
-                        puts("Dwin");
-                }
+                if(board[i]!=' '){
+                
+                        if(board[i]==board[i+2]){
+                                if(board[i+2]==board[i+4])
+                                puts("Dwin");
+                        }
+                
         } // checks diagonal wins
         for(i=0;i<3;i++){
-                if (board[i]==board[i+3]){
-                        if (board[i+6]==board[i+3]){
-                                puts("Vwin");
+                if(board[i]!=' '){
+                        if (board[i]==board[i+3]){
+                                if (board[i+6]==board[i+3]){
+                                        puts("Vwin");
+                                }
                         }
                 }
-        } // checks vertical wins
+        }// checks vertical wins
 }
