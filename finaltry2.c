2d5895b7504e40a89cfe0bf86bdf6f12d4ca2a03
cameron-klales <tuf62129@temple.edu>
2015-12-08 00:52:57 -0500

    Update finaltry2.c

--- finaltry2.c
+++ finaltry2.c
@@ -101,21 +101,21 @@ input, sets and returns z to capital x or o.
 void defaultmove(char board[], char j){
         if(board[0] != ' '){
                 board[0] = j;
-        }else if(board[2] != ' '){
+        }else if(board[2] == ' '){
                 board[2] = j;
-        }else if(board[6] != ' '){
+        }else if(board[6] == ' '){
                 board[6] = j;
-        }else if(board[8] != ' '){
+        }else if(board[8] == ' '){
                 board[8] = j;
-        }else if(board[4] != ' '){
+        }else if(board[4] == ' '){
                 board[4] = j;
-        }else if(board[3] != ' '){
+        }else if(board[3] == ' '){
                 board[3] = j;
-        }else if(board[5] != ' '){
+        }else if(board[5] == ' '){
                 board[5] = j;
-        }else if(board[1] != ' '){
+        }else if(board[1] == ' '){
                 board[1] = j;
-        }else if(board[7] != ' '){
+        }else if(board[7] == ' '){
                 board[7] = j;
         }else{
                 return;
